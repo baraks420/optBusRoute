6a9551fe23ca814460d15830a53c1d8c08247107
bsolomon <bsolomon@augury.com>
2023-06-25 09:58:11 +0300

    add number_of_same_values

--- opAlg/create_map.py
+++ opAlg/create_map.py
@@ -3,5 +3,5 @@ import numpy as np
 
 class StationsMap:
     def __init__(self, distances, exp):
-        self.distances = distances + distances.T
+        self.distances = distances + distances.T # creating the full matrix from triangular one (easier to make)
         self.exp = exp
